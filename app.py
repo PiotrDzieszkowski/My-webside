8438264f728c47103f7eb89020d1ff031ada960f
Piotr Dzieszkowski <piotr.dzieszkowski@gmail.com>
2022-04-12 11:13:46 +0100

    my_webside

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/message')
+def info():
+    return render_template("me.html")
+
+@app.route('/about_me', methods=["GET", "POST"])
+def about_me():
+    if request.method == "GET":
+        return render_template("about_me.html")
+    elif request.method == "POST":
+        print("POST received")
+        print(request.form)
+        return render_template("about_me.html")
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "GET":
+        return render_template("contact.html")
+    elif request.method == "POST":
+        print("POST received")
+        print(request.form)
+        return render_template("contact.html")
+
+@app.route('/home', methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("me.html")
+    elif request.method == "POST":
+        print("POST received")
+        print(request.form)
+        return render_template("me.html")
---
+++ templates/form.html.txt
@@ -0,0 +1,11 @@
+from flask import render_template
+
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("form.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return redirect("/")
